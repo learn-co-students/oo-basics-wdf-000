a0106dc1ba8d3e8bf429c98799cbe2f59ff21f0a
Isuru Vidanapathirana <isuru260@gmail.com>
2016-08-09 19:49:11 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,32 @@
 class Book
+  def initialize(title)
+    @title = title
+  end
+  def title
+    @title
+  end
+  def author=(name)
+    @name = name
+  end
+  def author
+    @name
+  end
+  def page_count=(pages)
+    @pages = pages
+  end
+  def page_count
+    @pages
+  end
+  def genre=(genre)
+    @genre = genre
+  end
+  def genre
+    @genre
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 
 end
+
+Book.new("And Then There Were None")
